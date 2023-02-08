a2afed3ec63ab075b97e1a31e2de80f6e06f7c7d
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro.local>
2023-02-08 15:41:02 -0600

    deploy problems tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,8 +3,6 @@ const daisyui = require('daisyui')
 module.exports = {
     theme: {
     },
-    daisyui:{
-    },
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     plugins: [daisyui],
 }
